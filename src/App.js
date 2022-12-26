c045ed99d424e44858bb5aaac2ad71a33273bb9a
sonalijain20 <sonalijn20@gmail.com>
2022-12-26 17:40:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import './App.css';
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }]
+  }
+])
+
+function App() {
+  return (
+    <div >
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
